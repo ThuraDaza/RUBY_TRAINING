9b95df56fb72ea1e29b11134fb6c8cea6fe78c98
Sensor <sensorgaming19@gmail.com>
2021-12-15 12:28:51 +0630

    ThuraDaza updated comment for tuto files

--- tutorial_01/tuto_01.rb
+++ tutorial_01/tuto_01.rb
@@ -2,7 +2,9 @@ class Chess
 
   def create_chess
     for i in 1..8 do
+      # print 8 numbers for a row
       8.times { |j| (j+i) %2 == 0? print("1\t") : print("0\t") }
+      # line beak
       puts ""
     end
   end
--- tutorial_02/tuto_02.rb
+++ tutorial_02/tuto_02.rb
@@ -4,19 +4,24 @@ class Triangle
     count = 11
     center = (count/ 2.0).ceil
 
+    # print stars row by row
     for i in 1..count do
+      # calcalute stars for rows
       new_i = i
       new_i = center - (i - center) if i > center
       star = (new_i-1) + new_i
 
+      # print spaces
       for j in 1..(count - star) do
         print " "
       end
 
+      # print stars
       for k in 1..star do
         print "* "
       end
 
+      # line break
       puts ""
     end
   end
