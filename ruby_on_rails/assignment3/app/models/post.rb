279d910e006245a8e037ecbb7238d4d2091aaf2c
Sensor <sensorgaming19@gmail.com>
2022-01-11 16:12:18 +0630

    update models

--- ruby_on_rails/assignment3/app/models/post.rb
+++ ruby_on_rails/assignment3/app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :user_name, presence: true
   validates :title, presence: true
   validates :description, presence: true
-  validates :public_flag, presence: true
+  validates :public_flag, inclusion: { in: [ true, false ] }
 end
